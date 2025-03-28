64ba1de0fc0af79461e2065a17c8437cb88c9174
Martyshe <sapognikmisha@gmail.com>
2025-03-28 13:39:27 +0100

    tesrt commit

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ function App() {
     <>
         <CardContainer>
             <ProductCard src={"https://s3-alpha-sig.figma.com/img/295b/9b3e/0cbd17e4381497547009ce81acc4eee3?Expires=1743984000&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=U24Iv1PcCi8f803KqCSp0jI7cOsadZ54SazgDrEYdZA56mdWhTiHwX24benwbUWMN04NttcFyjIeqDM1lHWLshzadi85u~baLghahkIkSaFBIHZRbSzrSSVRxCySa6oYCdv6ZBURwsRbAQQXpswH5xgUuKkk0X56EG14bta-vqcXhB~tDckgmDDfAQebmDDsnX-nZz2f~5L6y0xxXRwK~hAkdp5VK-mH5migbfgKwpizkGq1rtpjRa9RRSBwcod~VHXtQpmvErtO0un3Nch9i~YWq5TORhqXhSGOtFxjKul1ASBiakSGacVGUITaLvquCPaJ18UsNtrbzIPEGOMyqw__"}/>
+            <p>dklsk</p>
         </CardContainer>
     </>
   )
--- src/components/Card.styled.tsx
+++ src/components/Card.styled.tsx
@@ -62,7 +62,7 @@ const ProductCard: React.FC<ProductCardProps> = () => {
             <Image alt='landscape'
                    src={"https://s3-alpha-sig.figma.com/img/295b/9b3e/0cbd17e4381497547009ce81acc4eee3?Expires=1743984000&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=U24Iv1PcCi8f803KqCSp0jI7cOsadZ54SazgDrEYdZA56mdWhTiHwX24benwbUWMN04NttcFyjIeqDM1lHWLshzadi85u~baLghahkIkSaFBIHZRbSzrSSVRxCySa6oYCdv6ZBURwsRbAQQXpswH5xgUuKkk0X56EG14bta-vqcXhB~tDckgmDDfAQebmDDsnX-nZz2f~5L6y0xxXRwK~hAkdp5VK-mH5migbfgKwpizkGq1rtpjRa9RRSBwcod~VHXtQpmvErtO0un3Nch9i~YWq5TORhqXhSGOtFxjKul1ASBiakSGacVGUITaLvquCPaJ18UsNtrbzIPEGOMyqw__"}/>
             <Name>Header</Name>
-            <Description>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita, incidunt.</Description>
+            <Description>Lorem fgfgipsum dolor sit amet, consectetur adipisicing elit. Expedita, incidunt.</Description>
             <BtnContainer>
                 <Button primary>See more</Button>
                 <Button>Save</Button>
